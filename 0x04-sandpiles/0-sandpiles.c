10af09a2fd1cf260a5e099df92d3969b0c4ee540
anamariaroman <anamariaromanvalencia@gmail.com>
2022-01-17 11:24:58 +0700

    file fix

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -38,11 +38,11 @@ void sum(int src[3][3], int dest[3][3])
 		}
 }
 /**
-  * test - test
+  * tst - tst
   * @arr: matrix
   * Return: 0
   */
-int test(int arr[3][3])
+int tst(int arr[3][3])
 {
 	int x, y;
 
