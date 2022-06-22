800f0b39987dbd98cbc1579650a7dd582f4ad452
anamariaroman <anamariaromanvalencia@gmail.com>
2022-06-23 03:45:46 +0700

    changes

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,35 +1,30 @@
 #!/usr/bin/python3
 """
-Make change module
+Coin changer.
 """
 
 
 def makeChange(coins, total):
-    """Given a pile of coins of different values,
-    determine the fewest number of coins
-    needed to meet a given amount 'total'
-    Args:
-        coins ([list]): a list of the values
-        of the coins in your possession
-        total ([number]): amount
-    Return: fewest number of coins needed
-    to meet total
-    """
+    """Fewest number of coins needed to meet
+    a given amount total."""
+
     if total <= 0:
         return 0
 
     coins.sort(reverse=True)
+    stack = total
+    mv = 0
+    cnt = 0
 
-    i, ncoins = (0, 0)
-    cpy_total = total
-    len_coins = len(coins)
+    while (mv < len(coins)):
+        if stack == 0:
+            return cnt
 
-    while(i < len_coins and cpy_total > 0):
-        if (cpy_total - coins[i]) >= 0:
-            cpy_total -= coins[i]
-            ncoins += 1
+        if coins[mv] > stack:
+            mv += 1
+
         else:
-            i += 1
+            stack -= coins[mv]
+            cnt += 1
 
-    check = cpy_total > 0 and ncoins > 0
-    return -1 if check or ncoins == 0 else ncoins
+    return -1
